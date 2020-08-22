49acafa4c77d835cc9fcf56b04bde7d8e3b99f57
Abkreno <mohamedelhamamsy789@gmail.com>
2020-08-22 09:28:25 +0200

    Fix api /

--- server/config/express.js
+++ server/config/express.js
@@ -44,13 +44,13 @@ passport.use('facebook', strategies.facebook);
 passport.use('google', strategies.google);
 
 // mount api v1 routes
-app.use('api/v1', routes);
+app.use('/api/v1', routes);
 
 // if error is not an instanceOf APIError, convert it.
 app.use(error.converter);
 
 // catch 404 and forward to error handler
-// app.use(error.notFound);
+app.use(error.notFound);
 
 // error handler, send stacktrace only during development
 app.use(error.handler);
