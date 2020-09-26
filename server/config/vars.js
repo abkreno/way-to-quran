e9cbf1b653c3a6859065db07861ea9b452ff88fc
Abkreno <mohamedelhamamsy789@gmail.com>
2020-09-27 00:07:05 +0200

    Add HOST to prod env

--- server/config/vars.js
+++ server/config/vars.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const argv = require('./argv');
 
 // get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = argv.host || process.env.HOST;
+const customHost = process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 // // import .env variables
