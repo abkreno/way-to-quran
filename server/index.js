f0a5dcd5ddeb360846c9bd7b2ee0022d7f163cc6
Abkreno <mohamedelhamamsy789@gmail.com>
2020-09-27 22:41:20 +0200

    Update package.json

--- server/index.js
+++ server/index.js
@@ -1,16 +1,63 @@
-const express = require('express');
-const app = express();
+/* eslint-disable prefer-destructuring */
+/* eslint consistent-return:0 import/order:0 */
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 8080;
+const mongoose = require('./config/mongoose');
 
-// set the home page route
-app.get('/', (req, res) => {
-  // ejs render automatically looks in the views folder
-  res.send('Hello World');
+// open mongoose connection
+mongoose.connect();
+
+const logger = require('./config/logger');
+
+const argv = require('./config/argv');
+const setup = require('./middlewares/frontendMiddleware');
+const { port, host, prettyHost } = require('./config/vars');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const ngrok =
+  (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel
+    ? require('ngrok')
+    : false;
+const { resolve } = require('path');
+
+// If you need a backend, e.g. an API, add your custom backend-specific middleware here
+const app = require('./config/express');
+// app.use('/api', myApi);
+
+// In production we need to pass these values in instead of relying on webpack
+setup(app, {
+  outputPath: resolve(process.cwd(), 'build'),
+  publicPath: '/',
 });
 
-app.listen(port, () => {
-  console.log(`Our app is running on http://localhost:${port}`);
+// use the gzipped bundle
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz'; // eslint-disable-line
+  res.set('Content-Encoding', 'gzip');
+  next();
 });
+
+// Start your app.
+app.listen(port, host, async err => {
+  if (err) {
+    return logger.error(err.message);
+  }
+
+  // Connect to ngrok in dev mode
+  if (ngrok) {
+    let url;
+    try {
+      url = await ngrok.connect(port);
+    } catch (e) {
+      return logger.error(e);
+    }
+    logger.appStarted(port, prettyHost, url);
+  } else {
+    logger.appStarted(port, prettyHost);
+  }
+});
+
+/**
+ * Exports express
+ * @public
+ */
+module.exports = app;
